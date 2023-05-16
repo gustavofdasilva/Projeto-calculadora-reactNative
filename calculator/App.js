4176515fa0d2787f3c8d7098288f46ce72123c0f
Gustavo Ferreira da Silva <gustavofsilvas@gmail.com>
2023-05-15 22:11:43 -0300

    flatlist

--- calculator/App.js
+++ calculator/App.js
@@ -1,7 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
+
+
 
 export default function App() {
+  const buttonKeys = [
+    {keyCode: '1'},
+    {keyCode: '2'},
+    {keyCode: '3'},
+    {keyCode: '4'},
+    {keyCode: '5'},
+    {keyCode: '6'},
+    {keyCode: '7'},
+    {keyCode: '8'},
+    {keyCode: '9'},
+    {keyCode: '10'},
+    {keyCode: '11'},
+    {keyCode: '12'},
+    {keyCode: '13'},
+    {keyCode: '14'},
+    {keyCode: '15'},
+    {keyCode: '16'},
+  ]
+  
+  const ButtonCalc = ({keyCode}) => {
+    return (
+      <View style={styles.buttonView}>
+        <Text>{keyCode}</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.main}>
       <View style={styles.textInputView}>
@@ -9,6 +38,15 @@ export default function App() {
           style={styles.textInput}
           />
       </View>
+      <SafeAreaView
+        style={{flex:1}}>
+        <FlatList
+          contentContainerStyle={{flexGrow:1}}
+          data={buttonKeys}
+          renderItem={({item}) => <ButtonCalc keyCode={item.keyCode}/>}
+          numColumns={4}
+        />
+      </SafeAreaView>
     </View>
   );
 }
@@ -33,5 +71,14 @@ const styles = StyleSheet.create({
       marginBottom:10,
       fontSize: 40,
       padding: 10,
-    }
+    },
+
+  buttonsView: {
+      
+    },
+
+  buttonView: {
+    backgroundColor: '#DFF',
+    flexGrow: 1,
+  }
 });
